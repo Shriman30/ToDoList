4fbac0b3c60676aa02965985822261841c533fdb
Shriman Vinayagamoorthy <shriman30@hotmail.com>
2022-01-16 22:00:36 -0500

    To do list. Some small bugs which causes the add button to delay.. could be on my system only. Need to test on other machine as well for confirmation.

--- ToDoList/MainWindow.xaml.cs
+++ ToDoList/MainWindow.xaml.cs
@@ -24,5 +24,46 @@ namespace ToDoList
         {
             InitializeComponent();
         }
+
+       
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            btnAdd.IsEnabled = true;
+            lstBoxList.Items.Add(txtInput.Text);
+            txtInput.Text = "";
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            lstBoxList.Items.Remove(lstBoxList.SelectedItem); // delete the item selected on list menu
+            btnDelete.IsEnabled = false; // make the button unclickable.
+
+        }
+
+        private void lstBoxList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            btnDelete.IsEnabled = true;
+        }
+
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            // if nothing is written in the txt field, then disable add button
+            if(txtInput.Text.Length == 0)
+            {
+                btnAdd.IsEnabled = false;
+            }
+            // otherwise enable add button and proceed to taking user input and adding to the list
+            else
+            {
+                btnAdd.IsEnabled = true; // enable add button
+                if (e.Key == Key.Enter)
+                {
+                    lstBoxList.Items.Add(txtInput.Text);
+                    txtInput.Text = "";
+                }
+            }
+            
+        }
     }
 }
